21c2a7610018367cfb00214213a30eeecd0b6e9c
Zaw Hlaing Bwar <zawhlaingbwar@8bod.com>
2019-10-08 15:44:45 +0630

    {lazy load}

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter as Router,Switch, Route, Redirect } from 'react-router-dom';
 
-import PageOne from './pages/PageOne'
-import PageTwo from './pages/PageTwo'
-import PageThree from './pages/PageThree'
-import PageFour from './pages/PageFour'
+// import PageOne from './pages/PageOne'
+// import PageTwo from './pages/PageTwo'
+// import PageThree from './pages/PageThree'
+// import PageFour from './pages/PageFour'
+
+import Loading from './components/Loading'
+
+const PageOne = lazy(() => import('./pages/PageOne'));
+const PageTwo = lazy(() => import('./pages/PageTwo'));
+const PageThree = lazy(() => import('./pages/PageThree'));
+const PageFour = lazy(() => import('./pages/PageFour'));
 
 export default class App extends React.Component {
   render(){
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route component={PageOne} exact path="/page-one"/>
-            <Route component={PageTwo} exact path="/page-two"/>
-            <Route component={PageThree} exact path="/page-three"/>
-            <Route component={PageFour} exact path="/page-four"/>
-            <Redirect exact from="/" to="/page-one" />
-          </Switch>
+          <Suspense fallback={<Loading/>}>
+            <Switch>
+              <Route component={PageOne} exact path="/page-one"/>
+              <Route component={PageTwo} exact path="/page-two"/>
+              <Route component={PageThree} exact path="/page-three"/>
+              <Route component={PageFour} exact path="/page-four"/>
+              <Redirect exact from="/" to="/page-one" />
+            </Switch>
+          </Suspense>
         </Router>
       </div>
     );
